05ed05f4f15fad7f7bfd2ceb6d3b07e058dea953
Magnus Stråle <magnus.strale@factor10.com>
2019-07-10 20:34:24 +0200

    Change matrix style to struct+indexer

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,37 +1,10 @@
 use core::ops;
 //extern crate test;
 
-type R4 = [f64;4];
-
-type Matrix4 = [[f64; 4]; 4];
 type Matrix3 = [[f64; 3]; 3];
 type Matrix2 = [[f64; 2]; 2];
 
-fn mul(lhs: Matrix4, rhs: Matrix4) -> Matrix4 {
-    let mut m = [[0.0; 4]; 4];
-    for row in 0..4 {
-        for col in 0..4 {
-            let mut acc: f64 = 0.0;
-            for i in 0..4 {
-                acc += lhs[row][i] * rhs[i][col];
-            }
-            m[row][col] = acc;
-        }
-    }
-    m
-    //Matrix4::from(m)
-}
-
-#[derive(Debug, Clone)]
-struct Matrix {
-    size: usize,
-    m: Vec<Vec<f64>>
-}
-
-#[derive(Debug, Copy, Clone)]
-struct Matrix4x4 {
-    inner: [Row4; 4]
-}
+type R4 = [f64;4];
 
 #[derive(Debug, Copy, Clone)]
 struct Row4 {
@@ -45,10 +18,37 @@ impl ops::Index<usize> for Row4 {
     }
 }
 
-impl Matrix4x4 {
-    fn new(row0: R4, row1: R4, row2 : R4, row3 : R4) -> Matrix4x4
-    {
-        Matrix4x4 { inner: [ Row4 { innerRow: row0}, Row4 { innerRow: row1}, Row4 { innerRow: row2}, Row4 { innerRow: row3}]}
+impl PartialEq for Row4 {
+    fn eq(&self, other: &Self) -> bool {
+        const EPS: f64 = 0.00001;
+        for col in 0..4 {
+            if (self[col] - other[col]).abs() > EPS {
+                return false;
+            }
+        }
+        true
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Matrix4x4 {
+    inner: [Row4; 4]
+}
+
+impl ops::Mul<Matrix4x4> for Matrix4x4 {
+    type Output = Matrix4x4;
+    fn mul(self, rhs: Matrix4x4) -> Matrix4x4 {
+        let mut m = Matrix4x4::new_empty();
+        for row in 0..4 {
+            for col in 0..4 {
+                let mut acc: f64 = 0.0;
+                for i in 0..4 {
+                    acc += self[row][i] * rhs[i][col];
+                }
+                m.set(row, col, acc);
+            }
+        }
+        m
     }
 }
 
@@ -59,104 +59,37 @@ impl ops::Index<usize> for Matrix4x4 {
     }
 }
 
-impl PartialEq for Matrix {
+impl PartialEq for Matrix4x4 {
     fn eq(&self, other: &Self) -> bool {
-        const EPS: f64 = 0.00001;
-        if self.size != other.size {
-            return false;
-        }
-        for row in 0..self.size {
-            for col in 0..self.size {
-                if (self.m[row][col] - other.m[row][col]).abs() > EPS {
-                    return false;
-                }
+        for row in 0..4 {
+            if self[row] != other[row] {
+                return false;
             }
         }
         true
     }
 }
 
-impl From<Matrix> for Matrix4 {
-    fn from(item: Matrix) -> Self {
-        [[item.m[0][0], item.m[0][1], item.m[0][2], item.m[0][3]],
-        [item.m[1][0], item.m[1][1], item.m[1][2], item.m[1][3]],
-        [item.m[2][0], item.m[2][1], item.m[2][2], item.m[2][3]],
-        [item.m[3][0], item.m[3][1], item.m[3][2], item.m[3][3]]]
-    }
-}
+impl Matrix4x4 {
+    const EMPTY_ROW: R4 = [0.0, 0.0, 0.0, 0.0];
 
-impl From<Matrix4> for Matrix {
-    fn from(item: Matrix4) -> Self {
-        Matrix::new4(&item[0], &item[1], &item[2], &item[3])
-    }
-}
-
-impl From<Matrix3> for Matrix {
-    fn from(item: Matrix3) -> Self {
-        Matrix::new3(&item[0], &item[1], &item[2])
-    }
-}
-
-impl From<Matrix2> for Matrix {
-    fn from(item: Matrix2) -> Self {
-        Matrix::new2(&item[0], &item[1])
-    }
-}
-
-impl ops::Mul<Matrix> for Matrix {
-    type Output = Matrix;
-    fn mul(self, rhs: Matrix) -> Matrix {
-        let mut m = Matrix::new(4);
-        for row in 0..4 {
-            for col in 0..4 {
-                let mut acc: f64 = 0.0;
-                for i in 0..4 {
-                    acc += self.m[row][i] * rhs.m[i][col];
-                }
-                m.m[row][col] = acc;
-            }
-        }
-        m
-    }
-}
-
-impl Matrix {
-    pub fn new(size: usize) -> Matrix {
-        let row = vec![0.0; size];
-        Matrix { size, m: vec![row; size] }
+    fn new(row0: R4, row1: R4, row2 : R4, row3 : R4) -> Matrix4x4
+    {
+        Matrix4x4 { inner: [ Row4 { innerRow: row0}, Row4 { innerRow: row1}, Row4 { innerRow: row2}, Row4 { innerRow: row3}]}
     }
 
-    pub fn new2(row0: &[f64], row1: &[f64]) -> Matrix {
-        let mut m = Matrix::new(2);
-        m.m[0] = row0.to_vec();
-        m.m[1] = row1.to_vec();
-        m
+    fn new_empty() -> Matrix4x4 {
+        Matrix4x4::new(Matrix4x4::EMPTY_ROW, Matrix4x4::EMPTY_ROW, Matrix4x4::EMPTY_ROW, Matrix4x4::EMPTY_ROW)
     }
 
-    pub fn new3(row0: &[f64], row1: &[f64], row2: &[f64]) -> Matrix {
-        let mut m = Matrix::new(3);
-        m.m[0] = row0.to_vec();
-        m.m[1] = row1.to_vec();
-        m.m[2] = row2.to_vec();
-        m
+    fn set(&mut self, row: usize, col: usize, value: f64) {
+        self.inner[row].innerRow[col] = value;
     }
-
-    pub fn new4(row0: &[f64], row1: &[f64], row2: &[f64], row3: &[f64]) -> Matrix {
-        let mut m = Matrix::new(4);
-        m.m[0] = row0.to_vec();
-        m.m[1] = row1.to_vec();
-        m.m[2] = row2.to_vec();
-        m.m[3] = row3.to_vec();
-        m
-    }
-
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    //use test::Bencher;
-
 
     #[test]
     fn construct_4x4_matrix()
@@ -217,57 +150,39 @@ mod tests {
 
     #[test]
     fn matrix_equality_different_matrices() {
-        let a: Matrix4 = [
+        let a = Matrix4x4::new(
             [1.0, 2.0, 3.0, 4.0],
             [5.0, 6.0, 7.0, 8.0],
             [9.0, 8.0, 7.0, 6.0],
-            [5.0, 4.0, 3.0, 2.0]];
-        let b: Matrix4 = [
+            [5.0, 4.0, 3.0, 2.0]);
+        let b = Matrix4x4::new(
             [1.0, 2.0, 3.0, 4.0],
             [5.0, 6.0, 7.0, 8.0],
             [9.0, 8.0, 7.0, 6.0],
-            [5.0, 4.0, 3.0, 1.0]];
+            [5.0, 4.0, 3.0, 1.0]);
 
         assert_ne!(a, b);
     }
 
     #[test]
     fn multiplying_two_matrices() {
-        let a = [
+        let a = Matrix4x4::new (
             [1.0, 2.0, 3.0, 4.0],
             [5.0, 6.0, 7.0, 8.0],
             [9.0, 8.0, 7.0, 6.0],
-            [5.0, 4.0, 3.0, 2.0]];
-        let b: Matrix4 = [
+            [5.0, 4.0, 3.0, 2.0]);
+        let b = Matrix4x4::new(
             [-2.0, 1.0, 2.0, 3.0],
             [3.0, 2.0, 1.0, -1.0],
             [4.0, 3.0, 6.0, 5.0],
-            [1.0, 2.0, 7.0, 8.0]];
-        let expected: Matrix4 = [
+            [1.0, 2.0, 7.0, 8.0]);
+        let expected = Matrix4x4::new(
             [20.0, 22.0, 50.0, 48.0],
             [44.0, 54.0, 114.0, 108.0],
             [40.0, 58.0, 110.0, 102.0],
-            [16.0, 26.0, 46.0, 42.0]];
-        let a1 = Matrix::from(a);
-        let b1 = Matrix::from(b);
+            [16.0, 26.0, 46.0, 42.0]);
 
-        let result = a1 * b1;
-        assert_eq!(expected, mul(a, b));
+        let result = a * b;
+        assert_eq!(expected, result);
     }
-
-    // #[bench]
-    // fn array_based_matrix_multiplication(b: &mut Bencher) {
-    //     let a: Matrix4 = [
-    //         [1.0, 2.0, 3.0, 4.0],
-    //         [5.0, 6.0, 7.0, 8.0],
-    //         [9.0, 8.0, 7.0, 6.0],
-    //         [5.0, 4.0, 3.0, 2.0]];
-    //     let b: Matrix4 = [
-    //         [-2.0, 1.0, 2.0, 3.0],
-    //         [3.0, 2.0, 1.0, -1.0],
-    //         [4.0, 3.0, 6.0, 5.0],
-    //         [1.0, 2.0, 7.0, 8.0]];
-    //     let c = mul(a, b);
-    
-    // }
 }
