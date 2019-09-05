afa227e6ad7491bc2ccf2d23d6004e4c083d79f2
Magnus Stråle <magnus.strale@factor10.com>
2019-09-05 22:13:35 +0200

    Add ray_for_pixel to Camera

--- src/camera.rs
+++ src/camera.rs
@@ -1,6 +1,8 @@
-use std::f64::consts::FRAC_PI_2;
+use std::f64::consts::{FRAC_PI_2, FRAC_PI_4, SQRT_2};
 use super::approx_eq;
 use super::canvas::Canvas;
+use super::tuple::{Tuple, ORIGO};
+use super::ray::Ray;
 use super::matrix::{Matrix, IDENTITY_MATRIX};
 
 
@@ -35,6 +37,18 @@ impl Camera {
             half_height,
             canvas: Canvas::new(hsize, vsize) }
     }
+
+    pub fn ray_for_pixel(&self, px: usize, py: usize) -> Ray {
+        let xoffset = (px as f64 + 0.5) * self.pixel_size;
+        let yoffset = (py as f64 + 0.5) * self.pixel_size;
+        let world_x = self.half_width - xoffset;
+        let world_y = self.half_height - yoffset;
+        let pixel = self.transform.inverse().unwrap() * Tuple::point(world_x, world_y, -1.);
+        let origin = self.transform.inverse().unwrap() * ORIGO;
+        let direction = (pixel - origin).normalize();
+
+        Ray::new(origin, direction)
+    }
 }
 
 #[cfg(test)]
@@ -61,5 +75,33 @@ mod tests {
     fn pixel_size_vertical_canvas() {
         let c = Camera::new(125, 200, FRAC_PI_2, None);
         assert!(approx_eq(c.pixel_size, 0.01));
+    }
+
+    #[test]
+    fn construct_ray_through_center_of_canvas() {
+        let c = Camera::new(201, 101, FRAC_PI_2, None);
+        let r = c.ray_for_pixel(100, 50);
+
+        assert_eq!(r.origin, ORIGO);
+        assert_eq!(r.direction, Tuple::vector(0., 0., -1.));
+    }
+
+    #[test]
+    fn construct_ray_through_corner_of_canvas() {
+        let c = Camera::new(201, 101, FRAC_PI_2, None);
+        let r = c.ray_for_pixel(0, 0);
+
+        assert_eq!(r.origin, ORIGO);
+        assert_eq!(r.direction, Tuple::vector(0.66519, 0.33259, -0.66851));
+    }
+
+    #[test]
+    fn construct_ray_when_camera_transformed() {
+        let t = Matrix::rotation_y(FRAC_PI_4) * Matrix::translation(0., -2., 5.);
+        let c = Camera::new(201, 101, FRAC_PI_2, Some(t));
+        let r = c.ray_for_pixel(100, 50);
+
+        assert_eq!(r.origin, Tuple::point(0., 2., -5.));
+        assert_eq!(r.direction, Tuple::vector(SQRT_2 / 2., 0., -SQRT_2 / 2.));
     }
 }
