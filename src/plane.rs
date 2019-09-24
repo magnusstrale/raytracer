d8394b55b57ea253fc83b005a21954885d2b1430
Magnus Stråle <magnus.strale@factor10.com>
2019-09-24 21:35:02 +0200

    Implement material and transformation for Plane

--- src/plane.rs
+++ src/plane.rs
@@ -1,13 +1,17 @@
 use super::intersection::{Intersection, Intersections};
-use super::material::{Material, DEFAULT_MATERIAL};
+use super::material::Material;
 use super::matrix::{Matrix, IDENTITY_MATRIX};
 use super::ray::Ray;
-use super::shape::{BoxShape, Shape};
+use super::shape::{inverse_transform_parameter, BoxShape, Shape};
 use super::tuple::{Tuple, VECTOR_Y_UP};
 use std::any::Any;
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct Plane {}
+pub struct Plane {
+    inverse_transform: Matrix,
+    transform: Matrix,
+    material: Material,
+}
 
 impl Shape for Plane {
     fn box_clone(&self) -> BoxShape {
@@ -36,21 +40,25 @@ impl Shape for Plane {
     }
 
     fn material(&self) -> &Material {
-        &DEFAULT_MATERIAL
+        &self.material
     }
 
     fn transformation(&self) -> Matrix {
-        IDENTITY_MATRIX
+        self.transform
     }
 
     fn inverse_transformation(&self) -> Matrix {
-        IDENTITY_MATRIX
+        self.inverse_transform
     }
 }
 
 impl Plane {
-    pub fn new() -> Self {
-        Plane {}
+    pub fn new(material: Option<Material>, transform: Option<Matrix>) -> Self {
+        Self {
+            transform: transform.unwrap_or(IDENTITY_MATRIX),
+            inverse_transform: inverse_transform_parameter(transform),
+            material: material.unwrap_or(Material::default()),
+        }
     }
 }
 #[cfg(test)]
@@ -59,7 +67,7 @@ mod tests {
 
     #[test]
     fn normal_of_plane_is_constant() {
-        let p = Plane::new();
+        let p = Plane::new(None, None);
         let n1 = p.inner_normal_at(Tuple::point(0., 0., 0.));
         let n2 = p.inner_normal_at(Tuple::point(10., 0., -10.));
         let n3 = p.inner_normal_at(Tuple::point(-5., 0., 150.));
@@ -71,7 +79,7 @@ mod tests {
 
     #[test]
     fn intersect_with_ray_parallel_to_plane() {
-        let p = Plane::new();
+        let p = Plane::new(None, None);
         let r = Ray::new(Tuple::point(0., 10., 0.), Tuple::vector(0., 0., 1.));
         let xs = p.inner_intersect(r);
 
@@ -80,7 +88,7 @@ mod tests {
 
     #[test]
     fn intersect_with_coplanar_ray() {
-        let p = Plane::new();
+        let p = Plane::new(None, None);
         let r = Ray::new(Tuple::point(0., 0., 0.), Tuple::vector(0., 0., 1.));
         let xs = p.inner_intersect(r);
 
@@ -90,10 +98,9 @@ mod tests {
     fn box_plane(p: Plane) -> BoxShape {
         Box::new(p)
     }
-    
     #[test]
     fn ray_intersecting_plane_from_above() {
-        let p = Plane::new();
+        let p = Plane::new(None, None);
         let r = Ray::new(Tuple::point(0., 1., 0.), Tuple::vector(0., -1., 0.));
         let xs = p.inner_intersect(r);
 
@@ -101,10 +108,9 @@ mod tests {
         assert_eq!(xs[0].t, 1.);
         assert_eq!(&xs[0].object, &box_plane(p));
     }
-    
     #[test]
     fn below() {
-        let p = Plane::new();
+        let p = Plane::new(None, None);
         let r = Ray::new(Tuple::point(0., -1., 0.), Tuple::vector(0., 1., 0.));
         let xs = p.inner_intersect(r);
 
