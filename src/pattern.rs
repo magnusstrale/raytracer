f46c7f8e069fce30f1bb16c4f8dddcfcd4def457
Magnus Stråle <magnus.strale@factor10.com>
2019-10-23 16:13:43 +0200

    Add more test on Pattern abstraction

--- src/pattern.rs
+++ src/pattern.rs
@@ -98,6 +98,52 @@ mod tests {
     use super::*;
     use crate::color::{BLACK, WHITE};
     use crate::sphere::Sphere;
+    use crate::matrix::IDENTITY_MATRIX;
+
+    #[derive(Debug, Clone, PartialEq)]
+    struct TestPattern {
+        transform: Matrix,
+        inverse_transform: Matrix
+    }
+
+    impl TestPattern {
+        fn new(transform: Option<Matrix>) -> Self {
+            TestPattern {
+                transform: transform.unwrap_or_default(),
+                inverse_transform: inverse_transform_parameter(transform)
+            }
+        }
+
+        fn new_boxed(transform: Option<Matrix>) -> BoxPattern {
+            Box::new(TestPattern::new(transform))
+        }
+    }
+
+    impl Pattern for TestPattern {
+        fn box_clone(&self) -> BoxPattern {
+            Box::new((*self).clone())
+        }
+
+        fn as_any(&self) -> &dyn Any {
+            self
+        }
+
+        fn box_eq(&self, other: &dyn Any) -> bool {
+            other.downcast_ref::<Self>().map_or(false, |a| self == a)
+        }
+
+        fn transformation(&self) -> Matrix {
+            self.transform
+        }
+
+        fn inverse_transformation(&self) -> Matrix {
+            self.inverse_transform
+        }
+
+        fn inner_pattern_at(&self, pattern_point: Tuple) -> Color {
+            Color::new(pattern_point.x, pattern_point.y, pattern_point.z)
+        }
+    }
 
     #[test]
     fn create_stripe_pattern() {
@@ -151,10 +197,46 @@ mod tests {
     }
 
     #[test]
-    fn patter_with_object_and_pattern_transformation() {
+    fn stripes_with_object_and_pattern_transformation() {
         let o = Sphere::new(None, Some(Matrix::scaling(2., 2., 2.)));
         let pattern = StripePattern::new_boxed(WHITE, BLACK, Some(Matrix::translation(0.5, 0., 0.)));
         let c = pattern.pattern_at_shape(&o, Tuple::point(2.5, 0., 0.));
         assert_eq!(WHITE, c);
+    }
+
+    #[test]
+    fn default_pattern_transformation() {
+        let tp = TestPattern::new_boxed(None);
+        assert_eq!(tp.transformation(), IDENTITY_MATRIX);
+    }
+
+    #[test]
+    fn assign_transformation_to_pattern() {
+        let tp = TestPattern::new_boxed(Some(Matrix::translation(1., 2., 3.)));
+        assert_eq!(tp.transformation(), Matrix::translation(1., 2., 3.));
+    }
+
+    #[test]
+    fn pattern_with_object_transformation() {
+        let shape = Sphere::new_boxed(None, Some(Matrix::scaling(2., 2., 2.)));
+        let tp = TestPattern::new_boxed(None);
+        let c = tp.pattern_at_shape(&*shape, Tuple::point(2., 3., 4.));
+        assert_eq!(c, Color::new(1., 1.5, 2.));
+    }
+
+    #[test]
+    fn pattern_with_pattern_transformation() {
+        let shape = Sphere::new_boxed(None, None);
+        let tp = TestPattern::new_boxed(Some(Matrix::scaling(2., 2., 2.)));
+        let c = tp.pattern_at_shape(&*shape, Tuple::point(2., 3., 4.));
+        assert_eq!(c, Color::new(1., 1.5, 2.));
+    }
+
+    #[test]
+    fn pattern_with_object_and_pattern_transformation() {
+        let shape = Sphere::new_boxed(None, Some(Matrix::scaling(2., 2., 2.)));
+        let tp = TestPattern::new_boxed(Some(Matrix::translation(0.5, 1., 1.5)));
+        let c = tp.pattern_at_shape(&*shape, Tuple::point(2.5, 3., 3.5));
+        assert_eq!(c, Color::new(0.75, 0.5, 0.25));
     }
 }
